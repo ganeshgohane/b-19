a4fea53405abe33f5b2fa3d90dad60054b318555
ganesh <ganeshsgohane@gmail.com>
2022-04-05 13:40:47 +0530

    --- Auto-Git Commit ---

---
+++ seedjob2/seedjob23.groovy
@@ -0,0 +1,61 @@
+pipelineJob('scriptedpipeline_converted') {
+
+  description('')
+
+  displayName('scriptedpipeline_converted')
+
+  keepDependencies(false)
+
+  quietPeriod(0)
+
+  checkoutRetryCount(0)
+
+  disabled(false)
+
+  concurrentBuild(false)
+
+  configure { flowdefinition ->
+
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2683.vd0a_8f6a_1c263') {
+
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git@4.11.0') {
+
+        'configVersion'(2)
+
+        'userRemoteConfigs' {
+
+          'hudson.plugins.git.UserRemoteConfig' {
+
+            'url'('https://github.com/ganeshgohane/b-19.git')
+
+          }
+
+        }
+
+        'branches' {
+
+          'hudson.plugins.git.BranchSpec' {
+
+            'name'('*/master')
+
+          }
+
+        }
+
+        'doGenerateSubmoduleConfigurations'(false)
+
+        'submoduleCfg'(class:'empty-list')
+
+        'extensions'()
+
+      }
+
+      'scriptPath'('project-pipeline/scriptedpipeline.groovy')
+
+      'lightweight'(true)
+
+    }
+
+  }
+
+}
